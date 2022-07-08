c8c85485a8fd0d745f541d95a059010fd8aab28f
David VanLaningham <david.vanlaningham@bluemedora.com>
2022-07-08 10:12:55 -0400

    Chevron right icon

--- ui/src/components/Icons/index.tsx
+++ ui/src/components/Icons/index.tsx
@@ -37,8 +37,28 @@ export const ChevronUp: React.FC<IconProps> = (props) => {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
+      {...props}
     >
       <polyline points="18 15 12 9 6 15"></polyline>
+    </svg>
+  );
+};
+
+export const ChevronRight: React.FC<IconProps> = (props) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <polyline points="9 18 15 12 9 6"></polyline>
     </svg>
   );
 };
