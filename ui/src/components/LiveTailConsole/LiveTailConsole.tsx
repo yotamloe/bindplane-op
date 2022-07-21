a97e134acd075bff202754e40d82d5891e8818cb
David VanLaningham <david.vanlaningham@bluemedora.com>
2022-07-21 18:23:10 -0400

    some ui improvements

--- ui/src/components/LiveTailConsole/LiveTailConsole.tsx
+++ ui/src/components/LiveTailConsole/LiveTailConsole.tsx
@@ -1,10 +1,20 @@
 import { gql, OnSubscriptionDataOptions } from "@apollo/client";
-import { Stack } from "@mui/material";
+import {
+  Card,
+  Collapse,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 import {
   LivetailSubscription,
   useLivetailSubscription,
 } from "../../graphql/generated";
+import { ChevronDown } from "../Icons";
 import styles from "./live-tail-console.module.scss";
 import { LTSearchBar } from "./SearchBar";
 
@@ -13,6 +23,39 @@ interface Props {
 }
 
 type Record = LivetailSubscription["livetail"][0]["records"][0];
+
+const ROWS: Record[] = [
+  {
+    timestamp: Date.now().toString(),
+    a: "b",
+    foo: "bar",
+    blah: "foo",
+  },
+  {
+    timestamp: Date.now().toString(),
+    a: "b",
+    foo: "bar",
+    blah: "foo",
+  },
+  {
+    timestamp: Date.now().toString(),
+    a: "b",
+    foo: "bar",
+    blah: "foo",
+  },
+  {
+    timestamp: Date.now().toString(),
+    a: "b",
+    foo: "bar",
+    blah: "foo",
+  },
+  {
+    timestamp: Date.now().toString(),
+    a: "b",
+    foo: "bar",
+    blah: "foo",
+  },
+];
 
 gql`
   subscription livetail($ids: [String!]!, $filters: [String!]!) {
@@ -58,21 +101,53 @@ export const LiveTailConsole: React.FC<Props> = ({ ids }) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div className={styles.dt}>Time</div>
-        <div className={styles.lg}>Message</div>
-      </div>
       <div className={styles.console} ref={consoleRef} onScroll={() => {}}>
+        <div className={styles.header}>
+          <div className={styles.ch} />
+          <div className={styles.dt}>Time</div>
+          <div className={styles.lg}>Message</div>
+        </div>
         {rows.map((row) => (
-          <Stack direction="row">
-            <div className={styles.dt}>{row.timestamp}</div>
-            <div className={styles.lg}>{row.body}</div>
-          </Stack>
+          <LiveTailRow record={row} />
         ))}
       </div>
       <div ref={lastRowRef} />
 
       <LTSearchBar value={filters} onValueChange={handleFilterChange} />
     </div>
+  );
+};
+
+const LiveTailRow: React.FC<{ record: Record }> = ({ record }) => {
+  const { timestamp, ...rest } = record;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Card
+      onClick={() => setOpen((prev) => !prev)}
+      classes={{ root: styles.card }}
+    >
+      <Stack direction="row">
+        <div className={styles.ch}>
+          <ChevronDown className={styles.chevron} />
+        </div>
+        <div className={styles.dt}>{record.timestamp}</div>
+        <div className={styles.lg}>{record.body}</div>
+      </Stack>
+      <Collapse in={open}>
+        <div className={styles["table-container"]}>
+          <Table padding="none" size="small">
+            <TableBody>
+              {Object.entries(rest).map(([k, v]) => (
+                <TableRow>
+                  <TableCell className={styles.key}>{k}</TableCell>
+                  <TableCell className={styles.value}>{v as any}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </div>
+      </Collapse>
+    </Card>
   );
 };
